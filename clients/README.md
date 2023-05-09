666ef2020deef7fa8d2e4da56a9c34cc7c918052
andre <andrenguimbous6@gmail.com>
2023-05-09 10:33:52 +0100

    adding client microservice

---
+++ clients/README.md
@@ -0,0 +1,7 @@
+<<<<<<< HEAD
+# eatch_restaurant
+Microservice pour la gestion des differents restaurants
+=======
+# eatch_client
+Microservice pour le client du restaurant eatch
+>>>>>>> 080ee78 (Initial commit)
---
+++ clients/src/playground-1.mongodb.js
@@ -0,0 +1,13 @@
+// MongoDB Playground
+// Use Ctrl+Space inside a snippet or a string literal to trigger completions.
+
+// The current database to use.
+use("user_db");
+
+// Create a new document in the collection.
+db.getCollection("restaurants").insertOne({
+  id_resataurant: "A0001",
+  restaurant_name: "vhcfgcxf",
+  info: { twon: "fdfcwsfsdw", adress: "qssdqdq", logo: "sfqxrfsf" },
+  userId: "adzqffgg,",
+});
