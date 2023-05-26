830915699f2fe39c0e914259f3d779f4f572c6b6
Mariline240 <kafanomariline5@gmail.com>
2023-05-26 12:26:06 +0200

    tout

--- restaurants/src/models/restaurant.js
+++ restaurants/src/models/restaurant.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const restoSchema = new mongoose.Schema(
   {
     restaurant_name: { type: String, require: true, maxlength: 50 },
-    info: {
+    infos: {
       town: { type: String, require: true },
       address: { type: String, require: true },
       logo: { type: String, require: true },
