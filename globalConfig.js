eb7b23bc816d62fdbb4da1896ced60c8790128e3
andre <andrenguimbous6@gmail.com>
2023-07-28 17:17:34 +0100

    changing category.json path

--- globalConfig.js
+++ globalConfig.js
@@ -1,6 +1,7 @@
 const path = require("path");
 
-const jsonFilesPath = path.join(__dirname, "jsonfiles");
+//const jsonFilesPath = path.join(__dirname, "jsonfiles");
+const jsonFilesPath = path.join(__dirname, "gateway/public/datas");
 
 module.exports = {
   jsonFilesPath,
